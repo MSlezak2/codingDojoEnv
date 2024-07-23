67e654153f1bbf25a6ff15800114242208ab2c15
fokija <83609069+fokija@users.noreply.github.com>
2024-07-23 10:21:15 +0000

    Add configuration file for the debugger and small refactor after run program tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
 #include "snail.h"
 
 int main()
 {
+    //path: /workspaces/codingDojoEnv/out/build/linux-debug
+    //use terminal and go to the path as above and start program using ./run_program
+    std::cout << "testowy print" << std::endl;
     return 0;
 }
